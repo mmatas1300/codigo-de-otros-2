3f781421bae6e231bc342455a23e1c79c2f9f402
Mario Mata <mmatas1300@gmail.com>
2023-11-29 14:25:08 -0600

    corrección :'(

--- formulario.js
+++ formulario.js
@@ -1,66 +1,32 @@
-var formulario = document.querySelector("#form")
+const formulario = document.querySelector(".formulario")//se cambió la declaración por const | se cambio el nombre de la clase a .formulario
+const nombreRef=formulario.elements[0];//Las referencias se hicieron globales
+const edadRef = formulario.elements[1];
+const nacionalidadRef = formulario.elements[2];
 
-formulario.onsubmit = function(e) {
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+formulario.onsubmit = function(e) { 
+ 
+  e.preventDefault();//Se completo el metodo preventDefault
+  const nombre = nombreRef.value;//Se cambiaron a constantes y se añadió un nombre con mpas contexto
+  const edad = edadRef.value;
+  const nacionalidad = nacionalidadRef.options[nacionalidadRef.selectedIndex].value;
 
-  var nombre = n.value
-  var edad = e.value
-
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
-
-  if (nombre.length === 0) {
-    n.classList.add("error")
-  }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
-
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
+ if (validarDatos(nombre,edad,nacionalidad))//La validadción se realiza en una función
+    agregarInvitado(nombre, edad, nacionalidad);//Agregar Invitado se subio de nivel
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
-
 function agregarInvitado(nombre, edad, nacionalidad) {
+//el valor de la nacionalidad se obtiene directamenet de HTML
 
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
-  }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
+const listaInvitadosRef = document.getElementById("lista-de-invitados") // Se cambia a const y en html se crea el ID
 
-var lista = document.getElementById("lista-de-invitados")
-
-var elementoLista = document.createElement("div")
+const elementoLista = document.createElement("div")
 elementoLista.classList.added("elemento-lista")
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
+const spanNombre = document.createElement("span")
+const inputNombre = document.createElement("input")
+const espacio = document.createElement("br")
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
 elementoLista.appendChild(spanNombre)
@@ -95,3 +61,27 @@ elementoLista.appendChild(botonBorrar);
 botonBorrar.parentNode.remove()
   }
 }
+
+
+const validarDatos = (nombre,edad,nacionalidad) =>{
+  let isValid = true;
+  if (nombre.length === 0) {
+    nombreRef.classList.add("error")
+    isValid = false; 
+  }
+  else if (edad < 18 || edad > 120 || edad == null) {//Las validaciones se cambiaron por else if | se añadió el caso en el que no ingresa un numero
+    edadRef.classList.add("error")
+    isValid = false;
+  }//El ultimo caso es redundante, se cambio por el retorno de la función
+  return isValid;
+};
+
+
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
